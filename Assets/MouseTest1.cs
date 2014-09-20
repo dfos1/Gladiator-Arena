3c4060b2d923630f836899a2572df13a2bc08933
dfos1 <dfos1@umbc.edu>
2014-09-20 11:51:15 -0400

    We're getting there! Grid system works, and base menu works. We're now working on submenus.

---
+++ Assets/MouseTest1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTest1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(Input.GetMouseButtonDown(0))
+			Debug.Log("Pressed left click.");
+		if(Input.GetMouseButtonDown(1))
+			Debug.Log("Pressed right click.");
+		if(Input.GetMouseButtonDown(2))
+			Debug.Log("Pressed middle click.");
+		*/
+		//Event e = Event.current;
+
+		Debug.Log(Input.mousePosition);
+	}
+}
